b4c18f09f503edda15dcb78fcabab93dea55056f
Alexander Savin <whoamisavin@yandex.ru>
2025-08-31 13:55:00 +0000

    Изменил список на словарь и добавление по дате

--- my_prog_v1.py
+++ my_prog_v1.py
@@ -2,8 +2,10 @@ HELP = """
 help - начнает печатать справку по программу
 add - добавить задачу в список ( название задачи запрашиваем у пользователя).
 show - напечатать все добавленные задачи."""
-#Cоздаем список
-tasks = []
+#Cоздаем словарь
+tasks = {
+  
+}
 
 run = True
 
@@ -12,13 +14,22 @@ while run:
   if command == "help":
     print(HELP)
   elif command == "add":
+    date = input("Введите дату для добавления задачи: ")
+    
     task = input("Введите название задачи : " )
-    tasks.append(task)
-    print("Задача добавлена")
+    if date in tasks:
+      #Дата есть в словаре
+      #Добавляем в список задачу
+      task[date].append(task)
+    else:
+     # Если даты нет в словаре, то мы создаем пустой список, а затем добавляем запись
+      tasks[date] = []
+      tasks[date].append(task)
+    print("Задача ", task, "добавлена на дату ", date)
 
   elif command == "show":
     print(tasks)
   else:
     print("Неизвестная команда")
-    run = False
+    break
 print("Have a nice day")
